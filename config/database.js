21ddacb510b626c2969b1e8f869bb0dc002df00f
QudsiaZainab <qudsiazainab2002@gmail.com>
2024-09-03 06:44:39 +0500

    db

--- config/database.js
+++ config/database.js
@@ -99,11 +99,13 @@
       connection: {
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'tasks'),
-        user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', '123'),
-        schema: env('DATABASE_SCHEMA', 'public'), // Not required
-        ssl: false, // Disable SSL
+        database: env('DATABASE_NAME', 'tasks_h0ti'),
+        user: env('DATABASE_USERNAME', 'qudsia'),
+        password: env('DATABASE_PASSWORD', 'SoVTwx689num9MNcNPM87b5M4ctdbSKD'),
+        schema: env('DATABASE_SCHEMA', 'public'),
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', true),
+        },
       },
       debug: false,
     },
