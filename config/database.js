2048c77ae5a2422857ce865d0f7338f94339e3c3
QudsiaZainab <qudsiazainab2002@gmail.com>
2024-09-03 10:23:05 +0500

    c

--- config/database.js
+++ config/database.js
@@ -1,17 +1,20 @@
- module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
+
+  // strapi-api/config/database.js
+  module.exports = ({ env }) => ({
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      schema: env('DATABASE_SCHEMA'),
-      ssl: env.bool('DATABASE_SSL', false) ? {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false) // SSL config for self-signed certificates
-      } : false,
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'tasks_h0ti'),
+        user: env('DATABASE_USERNAME', 'qudsia'),
+        password: env('DATABASE_PASSWORD', 'SoVTwx689num9MNcNPM87b5M4ctdbSKD'),
+        schema: env('DATABASE_SCHEMA', 'public'),
+        ssl: env.bool('DATABASE_SSL', false) ? {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false) // SSL config for self-signed certificates
+        } : false,
+      },
+      debug: false,
     },
-    debug: false,
-  },
-});
+  });
+  
